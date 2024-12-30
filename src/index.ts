56037813a90a335dc94efe36e924b8121c8b03e3
Jaime <jaime5alvarez5h@gmail.com>
2024-12-30 21:56:02 +0100

    Update package.json to add create-embeddings script and modify index.ts to log 'Hello World'

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { DATABASE_URL } from './config';
-import { processPdf } from './lib/retrieval/pdf';
-import { generateEmbeddings } from './lib/ai/embeddings';
-const db = drizzle(DATABASE_URL);
 
-
-generateEmbeddings('./files/billetes_va03okx_va03rrp.pdf')
+console.log('Hello World')
---
+++ src/scripts/create-embeddings.ts
@@ -0,0 +1,3 @@
+import { generateEmbeddings } from "../lib/ai/embeddings";
+
+generateEmbeddings('./files/billetes_va03okx_va03rrp.pdf')
